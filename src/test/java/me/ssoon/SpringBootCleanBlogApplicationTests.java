222065d7d2bce501c8ffbbd12dafa683f7b40c3b
soohoonlee <jjinimania@gmail.com>
2018-07-04 10:30:33 +0900

    Initial commit

---
+++ src/test/java/me/ssoon/SpringBootCleanBlogApplicationTests.java
@@ -0,0 +1,16 @@
+package me.ssoon;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootCleanBlogApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
